d3fec3b09a5d9ff7f61acf34ace044d00dbb8326
David Coss (bohrn) <david@davecoss.com>
2012-02-15 19:18:51 -0600

    Last commit before moving to C.

--- clock.asm
+++ clock.asm
@@ -11,10 +11,7 @@
 	goto INIT
 	;
 	org 0x04
-	movlw 0xc4
-	movwf TMR0; Count from c4 to overflow (60secs) then interrupt
-	incf low buffer_7seg + LEFT_DISPLAY,F
-	bcf INTCON,2
+ISR_INCREMENT_TIME_OF_DAY_MAC TMR0,myStatus,alarmHours,hours,alarmMinutes,minutes,INC_MINUTES,END_OF_INTERRUPT
 END_OF_INTERRUPT 
 	retfie
 INTERRUPT_CHECKED_TIME return
@@ -26,6 +23,8 @@ INIT
 	movlw 0x0
 	movwf low buffer_7seg + RIGHT_DISPLAY
 	movwf low buffer_7seg + LEFT_DISPLAY
+	movlw 0x29
+	movwf accumulator
 	bcf STATUS,RP0
 	bcf STATUS,RP1;
 	;movf resetTMR0,W
@@ -59,18 +58,166 @@ INIT
 	clrf btn_buffer
 	clrf btn_pressed
 	clrf minutes
+	clrf hours
+
+	bsf myStatus,HEX_OCTAL_BIT
 	
-MAIN_LOOP movlw 0xc4
-	subwf TMR0,W
+MAIN_LOOP movf minutes,W
 	movwf buffer_7seg + RIGHT_DISPLAY
+	movf hours,W
+	movwf buffer_7seg + LEFT_DISPLAY
 	call DISPLAY_BINARY
 	call DISPLAY_7SEG
 	call DEBOUNCE_BUTTONS
+	btfsc EDIT_BUTTON
+	goto EDIT_MODE
 	goto MAIN_LOOP
+
+EDIT_MODE movf accumulator,W
+	movwf buffer_7seg + RIGHT_DISPLAY
+	call DISPLAY_7SEG
+	call DEBOUNCE_BUTTONS
+	btfsc dipControl,BTN8
+	goto MOV_INPUT_ACC
+UPDATE_EDIT_DISPLAY bcf dipControl,BTN8
+	movf dipControl,W
+	movwf buffer_7seg + LEFT_DISPLAY
+	btfss EDIT_BUTTON
+	goto MAIN_LOOP
+	goto EDIT_MODE
+MOV_INPUT_ACC movf dipControl,W
+	andlw 0x7f
+	movwf accumulator
+	movwf buffer_7seg + RIGHT_DISPLAY
+	movlw SET_MINUTES_CODE
+	xorwf accumulator,W
+	btfsc STATUS,Z
+	goto SET_MINUTES
+	movlw SET_HOURS_CODE
+	xorwf accumulator,W
+	btfsc STATUS,Z
+	goto SET_HOURS
+	movlw SET_ALARM_HOURS_CODE
+	xorwf accumulator,W
+	btfsc STATUS,Z
+	goto SET_ALARM_HOURS
+	movlw SET_ALARM_MINUTES_CODE
+	xorwf accumulator,W
+	btfsc STATUS,Z
+	goto SET_ALARM_MINUTES
+	movlw SET_DAY_CODE
+	xorwf accumulator,W
+	btfsc STATUS,Z
+	goto SET_DAY
+	movlw SET_MONTH_CODE
+	xorwf accumulator,W
+	btfsc STATUS,Z
+	goto SET_MONTH
+	movlw TOGGLE_ALARM_CODE
+	xorwf accumulator,W
+	btfsc STATUS,Z
+	goto TOGGLE_ALARM
+	movlw SHOW_DATE_CODE
+	xorwf accumulator,W
+	btfsc STATUS,Z
+	goto SHOW_DATE
+	movlw TOGGLE_HEX_OCTAL_CODE
+	xorwf accumulator,W
+	btfsc STATUS,Z
+	goto TOGGLE_HEX_OCTAL_BIT
+UNKNOWN_EDIT_CODE 
+EDIT_MODE_NEXT clrf accumulator
+	goto EDIT_MODE
+
+TOGGLE_HEX_OCTAL_BIT movf myStatus,W
+	movwf temp
+	btfsc myStatus,HEX_OCTAL_BIT
+	bcf temp,HEX_OCTAL_BIT
+	btfss myStatus,HEX_OCTAL_BIT
+	bsf temp,HEX_OCTAL_BIT
+	movf temp,W
+	call PUSH_STACK
+	movlw 0x8
+	btfsc temp,HEX_OCTAL_BIT
+	movlw 0x16
+	bsf myStatus,HEX_OCTAL_BIT ; so that we can read the hex in W-reg like it was decimal.
+	movwf buffer_7seg + RIGHT_DISPLAY
+	clrf buffer_7seg + LEFT_DISPLAY
+TOGGLE_HEX_OCTAL_BIT_LOOP call DISPLAY_7SEG
+	btfsc dipControl,BTN8
+	goto TOGGLE_HEX_OCTAL_BIT_LOOP
+	call POP_STACK
+	movwf myStatus
+	goto EDIT_MODE_NEXT
+
+SET_MINUTES movlw low minutes
+	call SET_MEMORY
+	goto EDIT_MODE_NEXT
+
+SET_HOURS movlw low hours
+	call SET_MEMORY
+	goto EDIT_MODE_NEXT
+
+SET_ALARM_MINUTES movlw low alarmMinutes
+	call SET_MEMORY
+	goto EDIT_MODE_NEXT
+
+SET_ALARM_HOURS movlw low alarmHours
+	call SET_MEMORY
+	goto EDIT_MODE_NEXT
+
+SET_DAY movlw low dateDay
+	call SET_MEMORY
+	goto EDIT_MODE_NEXT
+	
+SET_MONTH movlw low dateMonth
+	call SET_MEMORY
+	goto EDIT_MODE_NEXT
+
+SET_MEMORY movwf FSR
+	movf INDF,W
+	movwf buffer_7seg + LEFT_DISPLAY
+	call DISPLAY_7SEG
+	call Delay1s
+SET_MEMORY_LOOP movf dipControl,W
+	andlw 0x7f
+	movwf buffer_7seg + LEFT_DISPLAY
+	call DISPLAY_7SEG
+	btfss dipControl,BTN8
+	goto SET_MEMORY_LOOP
+	movf buffer_7seg + LEFT_DISPLAY,W
+	movwf INDF
+	goto Delay1s
+	
+TOGGLE_ALARM movf alarmHours,W
+	movwf buffer_7seg + LEFT_DISPLAY
+	movf alarmMinutes,W
+	movwf buffer_7seg + RIGHT_DISPLAY
+	btfsc myStatus,ALARM_ON_OFF_BIT
+	goto CLEAR_ALARM_BIT
+	bsf myStatus,ALARM_ON_OFF_BIT
+	goto END_TOGGLE_ALARM
+CLEAR_ALARM_BIT bcf myStatus,ALARM_ON_OFF_BIT	
+END_TOGGLE_ALARM call DISPLAY_7SEG
+	btfsc dipControl,BTN8
+	goto END_TOGGLE_ALARM
+	goto EDIT_MODE_NEXT
+
+SHOW_DATE movf dateMonth,W
+	movwf buffer_7seg + LEFT_DISPLAY
+	movf dateDay,W
+	movwf buffer_7seg + RIGHT_DISPLAY
+END_SHOW_DATE call DISPLAY_7SEG
+	btfsc dipControl,BTN8
+	goto END_SHOW_DATE
+	goto EDIT_MODE_NEXT
+
+
 
 ;bin 7-seg display macros
 ;CREATE_DISPLAY_MAC myStatus,minutes,hours,MAKE_PACKET,binaryMinute,binaryHours,controlPort,rightDisplay,SEG_VALUES,output,LOAD_PACKET,indicator,hexToOctal
-
+INCREMENT_TIME_MAC dateDay,dateMonth,minutes,hours,ISR_REST_TMR,NUMBER_OF_DAYS
+NUMBER_OF_DAYS_MAC pclTemp
 	;
 ERROR_RETURN nop;not sure what to do in case of error yet
 	return
@@ -110,14 +257,26 @@ DISPLAY_7SEG
 DISPLAY_7SEG_RIGHT
 	movf buffer_7seg + RIGHT_DISPLAY,W
 	andlw 0xf
+	btfss myStatus,HEX_OCTAL_BIT
+	andlw 0x7
 	call PUSH_STACK ; value to display
-	movlw RIGHT_DISPLAY ; left digit
+	movlw RIGHT_DISPLAY ; digit
 	call PUSH_STACK
-	movlw RIGHT_DISPLAY ; left 7seg
+	movlw RIGHT_DISPLAY ; 7seg
 	call PUSH_STACK
 	call DISPLAY_7SEG_NIBBLE
-	swapf buffer_7seg+RIGHT_DISPLAY,W
-	andlw 0xf
+	movlw 0xf0
+	btfss myStatus,HEX_OCTAL_BIT
+	movlw b'111000'
+	andwf buffer_7seg+RIGHT_DISPLAY,W
+	movwf temp7seg
+	bcf STATUS,C
+	rrf temp7seg,F
+	rrf temp7seg,F
+	rrf temp7seg,F
+	btfsc myStatus,HEX_OCTAL_BIT
+	rrf temp7seg,F
+	movf temp7seg,W
 	call PUSH_STACK
 	movlw LEFT_DISPLAY
 	call PUSH_STACK
