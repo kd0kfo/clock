da84ba79efc49df0356b18788b1e5e489c6029e2
David Coss (diracnix) <david@davecoss.com>
2012-02-18 14:30:06 -0600

    Moved poll input to sim_clock.c because it is hardware specific.

--- input.c
+++ input.c
@@ -8,31 +8,6 @@
 
 #include "picfs_error.h"
 
-char poll_input()
-{
-  int val = getch();
-  if(val == ERR)
-    return button_state;
-  
-  if(val == 0xa)
-    {
-      EDIT_BUTTON ^= 1;
-      if(EDIT_BUTTON)
-	edit_mode = 1;
-      else
-	edit_mode = 0;
-      return button_state;
-    }
-
-  if(val < '1' || val > '8')
-    return button_state;
-  
-  val -= '1';
-  val = 1 << val;
-  
-  button_state ^= (char)(val & 0xff);
-  
-}
 
 signed char do_command(char command)
 {
