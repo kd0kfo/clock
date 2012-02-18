91791f4a1db09ae274e7d4739dadd8a23e18b2d3
David Coss (diracnix) <david@davecoss.com>
2012-02-18 15:24:57 -0600

    Rearranged command enumeration

--- input.h
+++ input.h
@@ -7,21 +7,21 @@ extern char poll_input();
 char should_do_command();
 
 // commands
-enum {
-  MOVELWH = 0/* Move literal to upper nibble of accumulator */,
-  MOVELWL/* Move literal to lower nibble of accumulator */,
-  USE_HEX,
-  USE_BINARY,
-  SET_HOUR,
-  SET_MINUTES,
-  SET_SECONDS,
-  SET_MONTH,
-  SET_DAY,
-  SET_YEAR
-};
+#define USE_HEX 0
+#define USE_BINARY 1
+#define SET_HOUR 2
+#define SET_MINUTES 3
+#define SET_SECONDS 4
+#define SET_MONTH 5
+#define SET_DAY 6
+#define SET_YEAR 7
+#define MOVELWL 0x20// Move literal to lower nibble of accumulator
+#define MOVELWH 0x40// Move literal to upper nibble of accumulator
+
 
 #define RUN_COMMAND_MASK 0x80
-#define MOVELWH_MASK 0x40
-#define MOVELWL_MASK 0x20
+#define MOVELWH_MASK MOVELWH
+#define MOVELWL_MASK MOVELWL
+
 
 #endif
